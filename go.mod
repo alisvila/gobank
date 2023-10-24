bcb3158aafec844906c84cb36df29805624f0df0
alisvila <aliam1991@gmail.com>
2023-10-24 23:57:04 +0330

    add config file

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.21.3
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- storage.go
+++ storage.go
@@ -4,17 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
 	"time"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "38.54.13.174"
-	port     = 5432
-	user     = "postgres"
-	password = "superstrongpass"
-	dbname   = ""
+	"gopkg.in/yaml.v3"
 )
 
 type Storage interface {
@@ -28,8 +22,36 @@ type PostGressStorage struct {
 	db *sql.DB
 }
 
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Dbname   string `yaml:"dbname"`
+	}
+}
+
+func errorHandler() {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+	}
+}
+
 func newPostgress() (*PostGressStorage, error) {
-	pgsql := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	defer errorHandler()
+
+	f, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := Config{}
+	err = yaml.Unmarshal(f, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	pgsql := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", cfg.Database.Host, cfg.Database.Port, cfg.Database.User, cfg.Database.Password)
 	fmt.Println(pgsql)
 	db, err := sql.Open("postgres", pgsql)
 	if err != nil {
@@ -46,7 +68,7 @@ func newPostgress() (*PostGressStorage, error) {
 	query := "CREATE DATABASE gobank"
 	res, err := db.ExecContext(ctx, query)
 	if err != nil {
-		return nil, err
+		fmt.Print(err)
 	}
 	fmt.Println(res)
 
